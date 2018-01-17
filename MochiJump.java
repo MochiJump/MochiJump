2d4b9337dc7f8c1847a134afa020932d0968a571
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-16 18:22:26 -0700

    Update MochiJump.java
    still not getting the label to show up when I run this meaning that the panel is not actually there or visible?

--- MochiJump.java
+++ MochiJump.java
@@ -6,18 +6,21 @@ import javax.swing.*;
 
 // I need a better understanding of how JFrame works. That is really holding me back right now.
 
+
 public class MochiJump {
 
+	
+	
 	 public static void main(String[] args) {
 
 		 	// okay so this testPain was used to just setup the JFrame. I've added a label at the top which will
 		 	// give me an indication as to where mochi is in the game even if he isn't animated. Next step is to get him 
 		 	// into this JFrame.
-		 DogLogic dogLogic = new DogLogic();
+		 		TestClass testPain = new TestClass();
 				 JFrame frame = new JFrame ("Mochi Jump");
 				 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				 // I need to use a drawPanel component here in place of testPain in the future
-				 frame.getContentPane().add(new TestClass(), BorderLayout.CENTER);
+				 frame.getContentPane().add(testPain, BorderLayout.CENTER);
 				 // okay so to setup the size I want, I will just do that in the JFrame
 				 frame.setPreferredSize(new Dimension(500,500));
 				 frame.pack();
