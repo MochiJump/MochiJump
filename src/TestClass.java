9977590e29c835e6a2143349b63a1d06cda08264
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-17 19:21:16 -0700

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -14,23 +14,21 @@ public class TestClass extends JPanel {
 	private DrawCanvas canvas;
 	
 	public TestClass () {
-//	    canvas = new DrawCanvas();
-//	    this.setLayout(new BorderLayout());
-//	    this.add(canvas, BorderLayout.CENTER);
+	    canvas = new DrawCanvas();
 		
 //		animation = new Animation();
-		
-//		int x = 50;
-//		int y = 50;
-//		mochi = new Mochi(x,y,0,-5,21,14);
-		
-		
-		
+// using the getX method causes this to break
+//		float mochiX = mochi.getX();
+		int x = 50;
+		int y = 50;
+		mochi = new Mochi(x,y,0,-5,21,14);
 		JPanel testPain = new JPanel();
 //		testPain.add(canvas, BorderLayout.CENTER);
-		JLabel testLabel = new JLabel("Test");
+		JLabel testLabel = new JLabel("Float.toString(mochiX)");
 		testPain.add(testLabel, BorderLayout.NORTH);
 		testPain.setPreferredSize(new Dimension (500,500));
+		add (testPain);
+		// okay none of this is showing up. I think I'll create a separate project just to play with this without distractions.
 	}
 	class DrawCanvas extends JPanel{
 		// why do I need this override here?
