81f05d4cd8c764c4004eab5054155a0fd37e84b3
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-18 20:12:54 -0700

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,44 +1,48 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
+	
 
 public class TestClass extends JPanel {
 
-	private Mochi mochi;
-	private LevelMap levelMap;
-	private Animation animation;
-	
-	private DrawCanvas canvas;
+	LevelMap levelMap = new LevelMap();
+	ArrayList <Rectangle> plat = new ArrayList<Rectangle>();
+	Draw draw;
 	
 	public TestClass () {
-	    canvas = new DrawCanvas();
+		draw = new Draw();
 		
-//		animation = new Animation();
-// using the getX method causes this to break
-//		float mochiX = mochi.getX();
-		int x = 50;
-		int y = 50;
-		mochi = new Mochi(x,y,0,-5,21,14);
 		JPanel testPain = new JPanel();
-//		testPain.add(canvas, BorderLayout.CENTER);
-		JLabel testLabel = new JLabel("Float.toString(mochiX)");
+		JLabel testLabel = new JLabel(Integer.toString(plat.size()));
 		testPain.add(testLabel, BorderLayout.NORTH);
 		testPain.setPreferredSize(new Dimension (500,500));
+		testPain.add(draw);
 		add (testPain);
-		// okay none of this is showing up. I think I'll create a separate project just to play with this without distractions.
 	}
-	class DrawCanvas extends JPanel{
-		// why do I need this override here?
+	class Draw extends JPanel{
 		@Override
 		public void paintComponent (Graphics g) {
 			super.paintComponent(g);
-			levelMap.draw(g);
-			animation.draw(g);
-		}
+			plat = levelMap.getPlat();
+			Graphics2D g2 = (Graphics2D) g.create();
+			for (Rectangle next: plat) {
+				// added fillRect and set color 
+				g2.setColor(Color.MAGENTA);
+				g2.fill(next);
+				g2.draw(next);
+			
+		//	levelMap.draw(g);
+		//	animation.draw(g);
+				}
 		
-	}
+			}
 	
+	}
 }
