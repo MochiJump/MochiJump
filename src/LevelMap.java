58a34c43c9be127571416d46cc98138a158cc752
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-19 18:54:10 -0700

    Update LevelMap.java

--- src/LevelMap.java
+++ src/LevelMap.java
@@ -1,84 +1,56 @@
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class LevelMap extends JPanel {
-// I want to use this to set up boundaries for Mochi, what he can stand on and what he will run into, etc,
-// in order to do this I need to first define the correct variables and then create the constructors for it.
-// I'll use floor and wall x's and y's to do this.
-
-	int x, y, width, height;
-// I'm trying to create an object for the platform and then create a list to contain it;
-// this thing below here is a setter, but I have it setup as a constructor. I don't think this is right.
-// okay I'm certain this isn't right now. I need to create an actual setter method and make sure that I am using it as well as the
-// getter methods in my other classes.
-// as there is nothing that will change the value of the platform the setter method here really isn't that important.
-	public LevelMap (int x, int y, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;	
-	}
-	// hmm is this a good constructor for this class? Will I be able to access the arraylist inside of it to use with it
-	// in another class? Might be worth trying to use it in the testclass when I get home and have access to my IDE
-	public LevelMap (ArrayList array) {
-		for (Rectangle next: platlist) {
-			Rectangle p1 = next.getBounds();
-		}
-	}
-	public LevelMap () {
-		
-	}
-
-//	static Rectangle plat1 = new Rectangle (500, 0, 1000, 500);
-//	static Rectangle plat2 = new Rectangle (500-35, 250, 100, 10);
-//	static Rectangle plat3 = new Rectangle (500-70, 120, 100, 10);
 	
-	private ArrayList<Rectangle> platlist = new ArrayList<>();
-// let's see if I can turn this into something better also let's try to avoid static objects and classes if possible.
+
+public class TestClass extends JPanel {
+
+	LevelMap levelMap = new LevelMap();
+	ArrayList <Rectangle> plat = new ArrayList<Rectangle>();
+
 	
-	private void addPlat (int x, int y, int width, int height){
-	platlist.add (new Rectangle (x,y,width,height));	
-	}
-	// does this need to be inside a method like private void PlatSetup (){...}	      
-	private void platSetup() {
-	addPlat(500, 0, 1000, 500);
-	addPlat(500-35,250,100,10);
-	addPlat(500-70, 120, 100, 10);
-	addPlat(500-35*3,250,100,10);
-	}
-	// this getter needs to be tested, I think there something that is trying to call on this in mochi class but it now
-	// has a different name.
-	public ArrayList<Rectangle> getPlat(){
-		platSetup();
-		return this.platlist;
-	}
-
-
-		      
-//	static {
-//	platlist.add(new Rectangle (plat1));
-//	platlist.add(new Rectangle (plat2));
-//	platlist.add(new Rectangle (plat3));
-//	platlist.add(new Rectangle (500-35*3, 250, 100, 10));
-//	}
-
-// Maybe I should move the below to the animate class. Have one class to bring graphical reality to things?
-// at the very least this method needs to be used.	
+	public TestClass () {
+		plat = levelMap.getPlat();
+		Rectangle r1 = plat.get(1);
+		JPanel testPain = new JPanel();
+		JLabel testLabel = new JLabel(Integer.toString(plat.size()));
+		JLabel testLabel2= new JLabel (r1.toString());
+		testPain.add(testLabel, BorderLayout.NORTH);
+		testPain.add(testLabel2, BorderLayout.SOUTH);
+	//	testPain.setPreferredSize(new Dimension(600,600));
+		add (testPain);
 	
-//	public void draw (Graphics g) {
-		// adding paint component this doesn't like this because it doesn't extend JPanel
-//		super.paintComponent(g);
-//		platSetup();
-//		Graphics2D g2 = (Graphics2D) g.create();
-//		for (Rectangle next: platlist) {
-			// added fillRect and set color 
-//			g2.setColor(Color.MAGENTA);
-//			g2.fill(next);
-//			g2.draw(next);
+	
+
+	}
+
+//Well it seems all of my problems stemmed from having the override in another class....
+// It seems that the paint Component only works correctly in full screen, otherwise it draws strangely.
+	
+		@Override
+		public void paintComponent (Graphics g) {
+			super.paintComponent(g);
+			plat = levelMap.getPlat();
+
+			Graphics2D g2 = (Graphics2D) g.create();
 			
-//			}
-//	}
-
+			for (Rectangle next: plat) {
+				// added fillRect and set color 
+				g2.setColor( Color.MAGENTA);
+				g2.fill(next);
+				g2.draw(next);
+			
+		//	levelMap.draw(g);
+		//	animation.draw(g);
+				}
+		
+			}
+	
 }
