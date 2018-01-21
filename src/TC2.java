b1897e0943ae964eef765a75b383cc0a05a5b4d6
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-21 15:22:00 -0700

    Create TC2.java

---
+++ src/TC2.java
@@ -0,0 +1,9 @@
+
+
+// I'm adding a second test class "TC2" in here so I can safely copy everything from the Animation Class I've already written
+// and delete and add stuff without losing my original work.
+
+public class TC2 {
+
+
+}
