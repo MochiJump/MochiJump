5752ba316d58c3b37661d4449a176d2fd0db1123
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-01-25 18:57:12 -0700

    Update Mochi.java

--- src/Mochi.java
+++ src/Mochi.java
@@ -1,11 +1,7 @@
-// Project started at 0830 AM 12/31/17 taking 1st break at 10300. Taking 2nd real break at 1237.
-// This is making my head hurt... slow and steady wins the race I guess...
-// This class is going contain both the sprite and the animation for Mochi when he moves (runs and jumps) and rests
-// outside of this class we will need a main class, a map class
-// best to use key binding api for this
-// I'm going to call it quits today at 1651 (12/31). I've got to figure out a more efficient way of inputting and testing
-// parameters for boundary detection. The solution is to use .getBounds (part of the geom API). Implement it and we can get
-// this guy up and running.
+// Project started at 0830 AM 12/31/17 
+
+// This class is going contain variables, boundary rules, and the Actions for the Mochi Sprite
+// outside of this class we will need a main class, a map class, and an animation class
 
 // read this: https://tips4java.wordpress.com/2013/06/09/motion-using-the-keyboard/
 
@@ -31,11 +27,9 @@ public class Mochi {
 // may want to include a cap to speeds here.
 	private float speedX;
 	private float speedY;
-// might as well make a gravity value, i'll set it to -5
-	private float gravity = -5;
-// let's make inertia a thing:
-	
-// since he isn't a ball, we will need the sprite's height and width as well;
+// might as well make a gravity value, i'll set it to -5, find that -3 is actually a better speed with a refresh rate of 30.
+	private float gravity = -3;	
+// since he isn't a ball, we will need the sprite's height and width as well (right now the sprite is 14 px tall and 21 px wide)
 	private float sH;
 	private float sW;
 // I'm thinking I'll include boolan variables to indicate which animation to run!
@@ -45,12 +39,10 @@ public class Mochi {
 	boolean mRunL;
 	boolean mJumpR;
 	boolean mJumpL;
-// I should probably include setters and getters for each of these.
 // looks like I never create a reference variable to LevelMap here, let's fix that:
 	LevelMap levelMap = new LevelMap();
 	
-
-	// This shouldn't be an array list it should be a method that sets all these to false
+// This shouldn't be an array list it should be a method that sets all these to false
 	private void setActionToFalse () {
 		mRestR = false;
 		mRestL = false;
@@ -59,24 +51,13 @@ public class Mochi {
 		mJumpR = false;
 		mJump = false;
 	}
-	// now I need to be sure that I call this at somepoint!
-	
-	// okay I've got an arraylist getter here, will that work?
-	//public ArrayList <Boolean> getMochiState() {
-	//	return bMochiAction;
-	//}
-	// not sure this will work. so I'm turning it into a comment for now
-	// Yeah I didn't need that because the booleans aren't private.
 	
 //Should I use this to indicate whether a jump is taking place? Chu means middle of in Japanese
-	boolean jumpChu = false;	
-// I'll need a timer here for mochi's jump input.
-	
+	boolean jumpChu = false;		
 // looks like I'll need to add actions here
 	private Action MoveRightAct;
 	private Action MoveLeftAct;
-// I've got to change the two Jump actions into one, the above two boolean actions can stay, which one will be accessed must be written into the new single Action
-// class
+// I've got to change the two Jump actions into one
 	private Action JumpAct;
 	private Action RestRight;
 	private Action RestLeft;
@@ -86,7 +67,6 @@ public class Mochi {
 	// and then just call back to here.
 	InputMap im = component.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
 	ActionMap am = component.getActionMap();
-// I'll put the keybindings at the bottom
 	
  	Rectangle mochi = new Rectangle ((int)(x), (int)(y), (int)(sH), (int)(sW));
 // so the above mochi is a rectangle, now I'm going to create lines around the rectangle which will act as the boundary
@@ -98,7 +78,7 @@ public class Mochi {
 // ***** Remember to assign a value to sW (spriteWidth) and sH (spriteHeight)
 	
 
-// so this is going to set variables for the mochi's positions and speed
+// all of the below can go. This is a constructor that I don't need for anything in this program.
 	
 	public Mochi (float x, float y, float speedX, float speedY, float sH, float sW) {
 		this.x = x;
@@ -108,7 +88,7 @@ public class Mochi {
 		this.sH = sH;
 		this.sW = sW;
 	}
-// all of the below can go. This is a constructor that I don't need for anything in this program.
+// same here. I just didn't understand how setters and getters worked.
 	public Mochi (boolean mRestR, boolean mRestL, boolean mRunR, boolean mRunL, boolean mJumpR, boolean mJumpL) {
 		this.mRestR=mRestR;
 		this.mRestL=mRestL;
@@ -117,11 +97,11 @@ public class Mochi {
 		this.mJumpR=mJumpR;
 		this.mJumpL=mJumpL;
 	}
-// okay now we're going to add the getter's here and since 
-		public float getSpeedY(){
+// This is a good example of a getter, and it is neccessary for the animation class to work
+	public float getSpeedY(){
 			return this.speedY;
 	}
-	 // I'm going to add another one for testing purposes
+	 // I don't think I need this any more
 		public float getX() {
 			return this.x;
 		}
@@ -129,17 +109,6 @@ public class Mochi {
 	public Mochi () {
 		// and it totally works. I could probably just delete everything above in the other two keep them all together etc and it would work.
 	}
-	
-// attempt to add mochi sprite 
-	// I'm going to create a new class for animation, I'll combine keybinding and calling animation just below
-	// current code just below is placeholder
-	// turning it into a comment for now. I think it's okay to delete it.
-//	public void draw (Graphics g) {
-		// rectangles start at the top left corner, and everything in java is inverted i.e. o is top left and any integer moves
-		// beneath or to the right depending on the axis. 
-//		g.setClip(new Rectangle2D.Double((int)(x), (int)(y), (int)(sH), (int)(sW)));
-//		g.drawImage(ms, (int) (sH) , (int) (sW),null);
-//	}
 
 // okay what do we still need in this class?
 // we need boundry detection
@@ -153,20 +122,19 @@ public class Mochi {
 		// let's apply inertia here:
 		y = y+speedY;
 		x = x+speedX;
-		// ***** BELLOW THIS IS WHAT IS CAUSING PROBLEMS!!!!!
+		// review the below code
 		ArrayList<Rectangle> platlist = levelMap.getPlat();
 		for (Rectangle next: platlist) {
 			Rectangle p1 = next.getBounds();
 			// in order to use intersection we need to create mochi as a rectangle at the top
-			// I made a mistake, trying to create a new variable as part of the mochi constructor. Try not to repeat that.
+			// 
 			if (mochi.intersects(p1)) {
 			// so here is where we program what we want a result of a collision...
 			// what I want is for mochi to return to the state just prior to his collision.
 			// how do I program that?
 			// okay so I have to write a few more if statements here:
 			// I need to determine if the collision happened from above or below,
-			// or from left or from the right. I think I'll take a break here 1/3 1839;
-			// picking back up time to write the rest of the bounds for mochi;
+			// or from left or from the right. 
 				if (mright.intersects(p1)) {
 					y = p1.y - sW;
 				// I like getting to use basic algebra in real life.
@@ -202,12 +170,8 @@ public class Mochi {
 		am.put("MoveLeftAct", MoveLeftAct);
 		im.put(KeyStroke.getKeyStroke("released LEFT"), "RestLeft");
 		am.put("RestLeft", RestLeft);
-		//well I was going to use the keybinding to indicate whether mochi jumped right or left, however, as I'm looking at it there is no way to do this,
-		//I'll have to write it into the action class above! *******************
 		im.put(KeyStroke.getKeyStroke("UP"), "JumpAct" );
 		am.put("JumpAct", JumpAct);
-		//If I did all of this correctly then I just need to make a main class and put all of this in there. I will most likely need to come back here
-		// and determine what to make runnable for threading purposes.
 		
 	}
 	
@@ -215,14 +179,11 @@ public class Mochi {
 
 	class MoveRightAct extends AbstractAction{
 		public void actionPerformed (ActionEvent mr) {
-			// I'm trying to turn off all boolean indicators here
+			// I'm trying to turn off all boolean action indicators here
 			// should I include a check to see if jump is false before continuing?
-			for (int counter = 0; counter < bMochiAction.size(); counter++) {
-				// this is okay
 				setActionToFalse();
-			}
 			x ++;
-			//this speed thing could cause problems here...
+			//this speedX variable will likely need adjusting
 			speedX ++;
 		//Here I turn on the one that is true
 		// shouldn't this be bMochiAction.set(2, true); ****
@@ -232,12 +193,9 @@ public class Mochi {
 	}
 	class MoveLeftAct extends AbstractAction{
 		public void actionPerformed (ActionEvent ml) {
-			for (int counter = 0; counter < bMochiAction.size(); counter++) {
-				setActionToFalse();
-			}
+			setActionToFalse();
 			x --;
 			speedX --;
-			// bMochiAction.set(3, true); ****
 			mRunL = true;
 			
 		}
@@ -250,6 +208,7 @@ public class Mochi {
 			if (jumpChu = false) {
 				// when working with boolean this seems to be the best way to do things.
 				// do I want to use a timer here? or a counter?
+				// can always go back to a counter if this is difficult
 				long start_jump_time = System.currentTimeMillis();
 				long up_time = 500;
 				long up_time_end = start_jump_time + up_time;
