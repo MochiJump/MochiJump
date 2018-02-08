9c1c207ccbc3a340cce02b031f2ea7fff91b641d
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-02-07 19:42:47 -0700

    Update StartPause.java

--- src/StartPause.java
+++ src/StartPause.java
@@ -7,11 +7,13 @@ public class StartPause extends JPanel {
   
   public StartPause(){
     StartActionHelper startActionHelper = new StartActionHelper();
-    PauseActionHelper pauseActionHelper = new PauseActionHelper()'
+    PauseActionHelper pauseActionHelper = new PauseActionHelper();
    JPanel sPScreen = new JPanel();
     JButton start = new JButton();
-    JButton exit = new JButton();
+    JButton continue = new JButton();
     start.addActionListener(startActionHelper);
+    continue.addActionListener(pauseActionHelper);
+    
         
    add (sPScreen);
   }
@@ -25,14 +27,17 @@ public class StartPause extends JPanel {
   }
   private class StartActionHelper {
     public void actionPerformed (ActionEvent s){
+      // this is to start the game
       isStart = false;
     }
   }
   private class PauseActionHelper {
     public void actionPerformed (ActionEvent p{
-     isPause = false
-       //????
+     // here we just want the gameStart thread to continue
+     isPause = false;
     }
   }
+ // what else do we want here, a level restart button, which means we have to work with
+// serialized peices of the game. or just a way to set the variable values back to the beginning
 }
 
