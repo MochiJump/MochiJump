a4bb58a1ac9bba7185f5423e252e212efc574cc0
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-02-17 16:17:46 -0700

    Update StartPause.java

--- src/StartPause.java
+++ src/StartPause.java
@@ -1,16 +1,32 @@
-import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+
+import javax.swing.*;
 
 public class StartPause extends JPanel {
   
-  //May be worth running this on it's own to see things visually before attemping to tie through Dog Logic
-  
+
+/** Okay I'm going to give the StartPause class it's own thread to run on... or should I? The main issue is how to
+* "change screens" Do I want to actually flip between JPanels or do I want to have one panel that paints two
+* different screens?
+* 
+* either way I can start by getting this to work independantly in and then figure out how to ingerate it. */
+
+	
   /** To Do:
+  * give this class it's own thread
   * Format layout of the screen
   * add a selector icon that moves with keybinding
   * program keybinding to only allow movement within menu options
   * Add mouse listener
   * Make mochi button blink when clicked
-  * Add level editor button? that functionality is not implemented yet
   */
   
   boolean isStart;
@@ -42,62 +58,37 @@ public class StartPause extends JPanel {
   // I made this image, but it doesn't serve a purpose and may just need to be erased.
   Image pause = new ImageIcon("pause.png").getImage();
   // still need images attached to them
-  Image continue = newImageIcon("continue.png").getImage();
+  Image cont = new ImageIcon("continue.png").getImage();
   Image selector = new ImageIcon("createSelectorImage.png").getImage();
   // selector can me animated which would require creating a new animation method as well as multiple versions
   // of the selector image above
-  Image exit = new ImageIcone("exit.png").getImage();
+  Image exit = new ImageIcon("exit.png").getImage();
   
   // requires variables for draw method location as well as a method for getting the values for those variables
   
   public StartPause(){
-    StartActionHelper startActionHelper = new StartActionHelper();
-    ContinueActionHelper countinueActionHelper = new CountinueActionHelper();
-    ExitActionHelper exitActionHelper = new ExitActionHelper();
-    MochiButtonHelper mochiButtonHelper = new MochiButtonHelper();
-   JPanel sPScreen = new JPanel();
+	JPanel sPScreen = new JPanel();
     // getting the height and width of the screen:
     screenSize = sPScreen.getSize();
     maxHeight = screenSize.height;
-    maxWidth = screenSize.width;
-    JButton startButton = new JButton(start); //<-- makes the image continue a JButton!
-    // must set location of these buttons instead of relying on a layout
-    sbSize = startButton.getSize();
-    startButton.setBounds(maxWidth/2, maxHeight/2, sbSize.width, sbSize.height); // <-- should place it center of screen
-    //now just work out the layout you'd like to see
-    JButton continueButton = new JButton(continue);
-    cbSize = continueButton.getSize();
-    //set location
-    continueButton.setBounds();
-    //set mochiFace to eyeopen state
-    mochiFace = mochiFaceState1;
-    JButton mochiButton = new JButton(mochiFace);
-    mbSize = mochiButton.getSize();
-    //set location (this needs to go at the top)
-    mochiButton.setBounds();
-    startButton.addActionListener(startActionHelper);
-    continueButton.addActionListener(countinueActionHelper);
-    sPScreen.add(startScreenKeyInputs());
-    sPScreen.add(startButton)
-    sPScreen.add(continueButton)
-    
-        
+    maxWidth = screenSize.width;    
    add (sPScreen);
   }
   
   public void draw (Graphics g){
-    Graphics2D g2 = (Graphics2D) g.create();
+    Graphics2D mochiIcon = (Graphics2D) g.create();
+    Graphics2D startSelect = (Graphics2D) g.create();
+    Graphics2D contSelect = (Graphics2D) g.create();
     
-   // all additional graphical implementation must go here 
-    // g2.setClip();
-    //g2.drawImage();
+    //mochiIcon.setClip();
+    //mochiIcon.drawImage();
   }
   public boolean getIsStart(){
     return this.isStart;
   }
   
   public boolean getIsPause(){
-    returh this.isPause;
+    return this.isPause;
   }
   public void setPause(){
     isPause = this.isPause;
@@ -137,13 +128,9 @@ public class StartPause extends JPanel {
     // I guess we will make Mochi blink if this button is covered with the mouse
   }
   private class PauseActionHelper {
-    public void actionPerformed (ActionEvent p{
+    public void actionPerformed (ActionEvent p){
      // here we just want the gameStart thread to continue
      isPause = false;
     }
   }
- // what else do we want here, a level restart button, which means we have to work with
-// serialized peices of the game. or just a way to set the variable values back to the beginning
-               // ultimately the level editor option would go in here as well.
 }
-
