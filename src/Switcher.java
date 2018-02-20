d50b9c2205b186ac9cbb14be3c5ef8d32264498d
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-02-20 11:49:52 -0700

    Update Switcher.java

--- src/Switcher.java
+++ src/Switcher.java
@@ -1,5 +1,7 @@
 // I'm going to try to use a compound JPanel to hold the other two JPanels that we will switch between in this program.
 
+// interested to see what happens with the repaint() & @Override Paint methods doing this.
+
 // read:https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
 
 public class Switcher {
@@ -13,7 +15,21 @@ public class Switcher {
     switchControl();
   }
   
-  private switchControl(){
+  private void switchControl(){
+    Thread controlThread = new Thread(){
+      public void run (){
+        while (true){
+          //method for JPanel switcher goes here
+          try {
+            Thread.sleep(1000/refreshRate);
+          }catch(InterruptedException s) {
+            System.out.println("An error has occured in the Switcher class"):
+          }
+        }
+      }
+    };
+          
+        
     
   }
 
