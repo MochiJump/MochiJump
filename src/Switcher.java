d5106e8f0980ae0751e84f38e710e9b7190c9ed1
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-02-20 09:47:54 -0700

    Update Switcher.java

--- src/Switcher.java
+++ src/Switcher.java
@@ -1,5 +1,7 @@
 // I'm going to try to use a compound JPanel to hold the other two JPanels that we will switch between in this program.
 
+// read:https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
+
 public class Switcher {
   
   DogLogic dogLogic = new DogLogic();
