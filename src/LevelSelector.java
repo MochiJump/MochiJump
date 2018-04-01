1f6de12212099647b406bf47aa79fb1752d1ad81
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-03-31 18:16:04 -0600

    Update LevelSelector.java

--- src/LevelSelector.java
+++ src/LevelSelector.java
@@ -3,8 +3,10 @@ public class LevelSelector {
   dbName; // names database
   dbPassword; // password for Database
   dbUserName; // username for Database
+  dbLocation; // 
 
   // this class needs to house a GUI for selecting levels based off of what is available on the SQL database
+  // will probably require JPanel extention for the graphical component of level selection
   
   /** need to use SQL command:
   * USE dbName
