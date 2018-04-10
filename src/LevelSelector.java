dca096d369dcb71ceaee96c5e7a14afb98445188
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-10 17:54:57 -0600

    Update LevelSelector.java

--- src/LevelSelector.java
+++ src/LevelSelector.java
@@ -8,6 +8,8 @@ public class LevelSelector {
   // this class needs to house a GUI for selecting levels based off of what is available on the SQL database
   // will probably require JPanel extention for the graphical component of level selection
   
+  //https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
+  
   /** need to use SQL command:
   * USE dbName
   * GO
