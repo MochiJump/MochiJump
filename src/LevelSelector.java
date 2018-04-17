eb6c65045ac0ee8b28fdbc6b5c55d83312fcb396
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-04-17 10:34:30 -0600

    Update LevelSelector.java

--- src/LevelSelector.java
+++ src/LevelSelector.java
@@ -1,9 +1,9 @@
 public class LevelSelector {
   
-  dbName; // names database
-  dbPassword; // password for Database
-  dbUserName; // username for Database
-  dbLocation; // 
+  private dbName; // names database
+  private dbPassword; // password for Database
+  private dbUserName; // username for Database
+  private dbLocation; // 
 
   // this class needs to house a GUI for selecting levels based off of what is available on the SQL database
   // will probably require JPanel extention for the graphical component of level selection
@@ -11,6 +11,13 @@ public class LevelSelector {
   //https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
   
   //https://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
+  
+  MysqlDataSource dataSource = new MysqlDataSource();
+  dataSource.setUser(dbUserName);
+  dataSource.setPassword(dbPassword);
+  dataSource.setDatabaseName(dbName);
+  
+  //https://docs.oracle.com/javase/tutorial/jdbc/basics/sqldatasources.html
   
   /** need to use SQL command:
   * USE dbName
