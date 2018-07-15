81a20d7d2581e143e6caaa39607cbfbbbb2276c9
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-07-15 11:05:58 -0600

    Update LevelSelector.java

--- src/com/MochiJump/LevelSelector.java
+++ src/com/MochiJump/LevelSelector.java
@@ -5,9 +5,21 @@
 package com.MochiJump;
 
 
-public class LeveSelector extends JPanel{
+public class LevelSelector extends JPanel{
+  JPanel lSPanel = new JPanel();
+  
 // will need to create and import images for ui
 // http://www.java-gaming.org/index.php/topic,13599.0
 // https://stackoverflow.com/questions/43205942/how-to-display-zoom-cubes-in-3d-in-javas-frames-panel
 
+ public LevelSelector(){
+   // to add keybinding lSPanel.add(componentwithkeybindingassociated);
+   setupSelection();
+   add(lSPanel);
+   
+ }
+  public void setupSelection(){
+    
+  }
+  
 }
